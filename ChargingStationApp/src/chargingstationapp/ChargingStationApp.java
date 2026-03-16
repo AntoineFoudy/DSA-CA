e2911736ce2bf20fc40e69d59e1457e698aa1642
AntoineFoudy <145511347+AntoineFoudy@users.noreply.github.com>
2026-03-16 14:54:37 +0000

    GUI and add Station
    GUI has been fully designed
    You can now add stations to a location/node

--- ChargingStationApp/src/chargingstationapp/ChargingStationApp.java
+++ ChargingStationApp/src/chargingstationapp/ChargingStationApp.java
@@ -15,6 +15,8 @@ public class ChargingStationApp {
      */
     public static void main(String[] args) {
         
+        GUI gui = new GUI();
+        gui.setVisible(true);
         
         
         /*
